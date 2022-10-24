a422536e854508f9a940a0b9289e44efdad19ba1
Juliana Souto <barutotec@gmail.com>
2022-10-24 20:41:42 -0300

    Foi inserido a imagem na tela principal do projeto

--- moedas.py
+++ moedas.py
@@ -2,8 +2,13 @@ from tkinter import *
 
 tela = Tk()
 tela.title('Conversor de Moedas')
-tela.geometry('500x500+400+200')
+tela.geometry('500x500+400+150')
 tela.resizable(False, False)
 
+logo = PhotoImage(file='C:\Projetos\conversor_de_moedas\img\images.png')
+logo = logo.subsample(1,1)
+img1 = Label(image=logo)
+
+img1.place(x=100, y=50)
 
 tela.mainloop()
