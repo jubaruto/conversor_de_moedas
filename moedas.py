e0a2b186eebad11603ac53847d8375c4cde0b36a
Juliana Souto <barutotec@gmail.com>
2022-10-26 17:09:48 -0300

    Foi inserido o conversor para a moeda Euro

--- moedas.py
+++ moedas.py
@@ -6,39 +6,61 @@ from tkinter import messagebox
 from numpy import real
 from requests import *
 
-url = get('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+url = get('https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL')
 url_format = url.json()
 
 dolarhj = url_format['USDBRL']['bid']
 dolarhj = float(dolarhj)
 dolarhj = round(dolarhj, 2)
 
-
+eurohj = url_format['EURBRL']['bid']
+eurohj = float(eurohj)
+eurohj = round(eurohj, 2)
 
 # TELA
 tela = Tk()
 tela.title('Conversor de Moedas')
-tela.geometry('300x450+500+150')
+tela.geometry('300x600+500+50')
 tela.resizable(False, False)
 tela.config(bg='#32CD32')
 
 # FUNÇÕES
 def conversao():
     try:
-        if textoreal.get() == '':
+        if textoreal.get() == '' and textoeuro.get() == '':
             dolar = float(textodolar.get())
+            
             real = dolar * dolarhj
             textoreal.insert(0, round(real, 2))
-        else:
+            
+            euro = real / eurohj
+            textoeuro.insert(0, round(euro, 2))
+            
+        elif textodolar.get() == '' and textoeuro.get() == '':
             real = float(textoreal.get())
+            
             dolar = real / dolarhj
             textodolar.insert(0, round(dolar, 2))
+            
+            euro = real / eurohj
+            textoeuro.insert(0, round(euro, 2))
+            
+        elif textoreal.get() == '' and textodolar.get() == '':
+            euro = float(textoeuro.get())
+            
+            real = euro * eurohj
+            textoreal.insert(0, round(real, 2))
+            
+            dolar = real / dolarhj
+            textodolar.insert(0, round(dolar, 2))
+            
     except ValueError:
         messagebox.showerror('Atenção!!', 'Insira um valor!')
 
 def limpar():
     textodolar.delete(0, END)
     textoreal.delete(0, END)
+    textoeuro.delete(0, END)
 
 logo = PhotoImage(file='C:\Projetos\conversor_de_moedas\img\imagem1.png')
 logo = logo.subsample(3, 3)
@@ -58,6 +80,10 @@ framereal = Frame(tela, borderwidth=1.5, relief='solid',  bg='#32CD32')
 labelreal = Label(tela, text='Real', bg='#32CD32')
 textoreal = Entry(framereal, width=45,)
 
+frameeuro = Frame(tela, borderwidth=1.5, relief='solid',  bg='#32CD32')
+labeleuro = Label(tela, text='Euro', bg='#32CD32')
+textoeuro = Entry(frameeuro, width=45,)
+
 bconversao = Button(tela, text='Conversão', font=('Georgia', 15),
                     highlightthickness=0, bd=0, bg='#32CD32', borderwidth=1.5, relief='solid', command=conversao)
                     
@@ -74,8 +100,12 @@ framereal.place(x=5, y=258, width=285, height=48)
 labelreal.place(x=10, y=260)
 textoreal.place(x=5, y=20)
 
-bconversao.place(x=90, y=350)
-blixo.place(x=270, y=320)
+frameeuro.place(x=5, y=333, width=285, height=48)
+labeleuro.place(x=10, y=335)
+textoeuro.place(x=5, y=20)
+
+bconversao.place(x=90, y=500)
+blixo.place(x=270, y=490)
 
 
 
